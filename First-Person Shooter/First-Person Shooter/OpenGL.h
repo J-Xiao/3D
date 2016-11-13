8639e46958c158d0826b94c615473ab3aab05f6a
J-Xiao <xiaojijundavid@gmail.com>
2016-11-13 18:00:13 -0500

    OpenGL class header file

--- First-Person Shooter/First-Person Shooter/OpenGL.h
+++ First-Person Shooter/First-Person Shooter/OpenGL.h
@@ -1,8 +1,19 @@
+#include "stdafx.h"
+
 #pragma once
+
 class OpenGL
 {
 public:
 	OpenGL();
-	~OpenGL();
+	virtual ~OpenGL();
+
+	HDC hDC;
+	HGLRC hRC;
+
+	BOOL SetupPixelFormat(HDC hDC);
+	void init(int width, int height);
+	void Render();
+	void CleanUp();
 };
 
